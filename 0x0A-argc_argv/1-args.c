6f6d10d9367c88fae6d49d554bcd20d64bd4d61c
GloriaBas <gloudos@yahoo.com>
2023-05-12 16:26:09 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A program that prints the number of arguments passed into it.
+ * @argv: argument vector
+ * @argc: number of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
