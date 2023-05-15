c370d372b9f916c041a86366738aa4a12b2b9966
GloriaBas <gloudos@yahoo.com>
2023-05-15 06:02:19 -0700

    100-atoi.c

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _atoi - a function that convert a string to an integer.
+ * @s: parameter
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int digit = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			digit = (digit * 10) + (*s - '0');
+		else if (digit > 0)
+			break;
+	} while (*s++);
+
+	return (digit * sign);
+}
