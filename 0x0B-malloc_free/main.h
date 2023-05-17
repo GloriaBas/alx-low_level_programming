18e6de110307694f947e18b198d7e8c6a38ff7a9
GloriaBas <gloudos@yahoo.com>
2023-05-17 07:02:23 -0700

    main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 char *create_array(unsigned int size, char c);
-int_putchar(char c);
+int _putchar(char c);
 char *_strdup(char *str);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
