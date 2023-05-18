e364f6a82c412a2457c66e00310df630d46ebf83
GloriaBas <gloudos@yahoo.com>
2023-05-18 14:54:59 -0700

    main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifdef MAIN_H
+#define MAIN_H
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int _putchar(char c);
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+
+
+
+
+
+
+#endif
