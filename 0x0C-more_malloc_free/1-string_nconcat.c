59696ceb3a1ea56ff401b5a52aa522efabe069c4
GloriaBas <gloudos@yahoo.com>
2023-05-19 13:32:45 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,48 +10,33 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int x;
-	char *str;
-	unsigned int y, len1, len2;
+	char *s;
+	unsigned int x = 0, y = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	if (s2 == NULL)
-		s2 = "";
-	
-	if (n >= len2)
-		n = len2;
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	str = malloc(sizeof(char) * (len1 + n + 1));
-	if (str == NULL)
+	if (!s)
 		return (NULL);
 
-	for (x = 0; s1[x] != '\0'; x++)
-		str[x] = s1[x];
-	for (y =0; y < n; y++)
-		str[x + y] = s2[y];
-	str[x + y] = '\0';
+	while (x < len1)
+	{
+		s[x] = s1[x];
+		x++;
+	}
+	while (n < len2 && x < (len1 + n))
+		s[x++] = s2[y++];
 
-	return (str);
-}
+	while (n >= len2 && x < (len1 + len2))
+		s[x++] = s2[y++];
+	s[x] = '\0';
 
-#include <stdlib.h>
-#include "main.h"
-/**
- * lenght - Calculates the lenght of a string
- * @s1: string
- * Return: lenght
- */
-
-unsigned int lenght(char *s1)
-{
-	unsigned int x;
-
-	if (s1 == NULL)
-		return (0);
-	for (x = 0; s1[x] != '\0'; x++)
-	{}
-
-	return (x);
+	return (s);
 }
